db580cd8425c96fda7d84bba2eb560e62bbf6322
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2024-01-15 11:37:14 +0200

    server initial upload - no adjustments

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const cors = require('./middlewares/cors');
+const authController = require('./controllers/authController');
+const entryController = require('./controllers/entryController');
+const trimBody = require('./middlewares/trimBody');
+const session = require('./middlewares/session');
+const ccController = require('./controllers/ccController');
+const pcController = require('./controllers/pcController');
+
+
+const connectionString = 'mongodb://localhost:27017/budget-stay-hub';
+
+start();
+
+async function start() {
+    await mongoose.connect(connectionString);
+    console.log('Database connected');
+
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+    app.use(trimBody());
+    app.use(session());
+
+    app.get('/', (req, res) => {
+        res.json({ message: 'REST service operational' });
+    });
+
+    app.use('/users', authController);
+    app.use('/entries', entryController);
+    app.use('/currency-converter', ccController)
+    app.use('/calchistories', pcController)
+    app.use('*', (req, res) => {
+        res.json(404)
+    })
+
+    app.listen(3030, () => console.log('REST service started'));
+}
