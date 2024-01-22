9da71385c91c5afc4db09d3232e2ef58fa06697d
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2024-01-22 16:05:55 +0200

    register routing added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/pages/login/login.component';
+import { RegisterComponent } from './core/pages/register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, pathMatch: 'full'},
+  { path: 'register', component: RegisterComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
