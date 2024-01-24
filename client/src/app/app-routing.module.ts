dcb68536e07cd1012c7b8227db392e3c1af10ec7
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2024-01-24 20:22:50 +0200

    reservation app-routing added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/pages/login/login.component';
 import { RegisterComponent } from './core/pages/register/register.component';
+import { ReservationComponent } from './core/pages/reservation/reservation.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, pathMatch: 'full'},
   { path: 'register', component: RegisterComponent, pathMatch: 'full'},
+  { path: 'reservation', component: ReservationComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
