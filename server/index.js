77fae8194a4b06a146413ff099988a03ee916875
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2024-01-29 17:19:03 +0200

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -6,8 +6,6 @@ const authController = require('./controllers/authController');
 const entryController = require('./controllers/entryController');
 const trimBody = require('./middlewares/trimBody');
 const session = require('./middlewares/session');
-const ccController = require('./controllers/ccController');
-const pcController = require('./controllers/pcController');
 
 
 const connectionString = 'mongodb://localhost:27017/budget-stay-hub';
@@ -31,8 +29,6 @@ async function start() {
 
     app.use('/users', authController);
     app.use('/entries', entryController);
-    app.use('/currency-converter', ccController)
-    app.use('/calchistories', pcController)
     app.use('*', (req, res) => {
         res.json(404)
     })
