4f5c4be7b7310d235f115ac164524e6865154b17
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2024-02-05 17:46:36 +0200

    ts logout function created in navbar component

--- client/src/app/shared/navbar/navbar.component.ts
+++ client/src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-// import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { SessionService } from 'src/app/core/services/session.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(
+    public authService: AuthService,
+    private sessionService: SessionService,
+    private router: Router) { }
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: (data) => {
+        //console.log(data);
+        this.sessionService.clearSession();
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 
 }
