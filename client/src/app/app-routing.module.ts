da076446afb9467f27ed96a19c1666f07a70c1ac
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2024-02-19 16:47:30 +0200

    contacts component added to app-routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 import { ReservationComponent } from './features/reservation/reservation.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, pathMatch: 'full'},
   { path: 'register', component: RegisterComponent, pathMatch: 'full'},
   { path: 'reservation', component: ReservationComponent, pathMatch: 'full'},
+  { path: 'contacts', component: ContactsComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
